dbfd64c84db5667a8e3d509209ca36da55d76872
AndySil23 <124683147+AndySil23@users.noreply.github.com>
2023-02-25 16:44:50 -0300

    Criado rep da atividade de condição
    Criado rep da atividade de condição

---
+++ Andy Silva - ATV1 Alessandro/JS/eventos.js
@@ -0,0 +1,9 @@
+// Faça um algoritmo para receber um número qualquer e informar na tela se é par ou ímpar.
+
+let numeroConfuso = (document.getElementByID("#numeroConfuso"))
+
+function revelarNumero = (numeroConfuso) {
+    if ((numeroConfuso / 2) % 0) {
+        alert('o número ("numeroConfuso") é par')
+    }
+}
